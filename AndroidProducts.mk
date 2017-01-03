4c1dec16330b63a0f1ba5abcd7e949e1abdae192
Kevin <biojoutk1106@gmail.com>
2017-01-03 19:51:36 +0100

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_zeroltexx.mk \
+    $(LOCAL_DIR)/cm.mk
---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zeroltexx/full_zeroltexx.mk)
+
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Galaxy S6 edge" \
+    TARGET_DEVICE="Galaxy S6 edge"
+
+PRODUCT_NAME := cm_zeroltexx
+PRODUCT_DEVICE := zeroltexx
